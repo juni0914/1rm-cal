26735598ecc17565b49b3dd76cda8fbec8ae545e
juni0914 <100837725+juni0914@users.noreply.github.com>
2022-03-12 01:48:52 +0900

    2022-03-12 / 01:47
    리스트 위 텍스트에 js 변수를 담기 위헤 참조 방식으로 값을 넘겨줌. (더 공부해야 됨.)

--- 1RMCal-main/js/1rmmain.js
+++ 1RMCal-main/js/1rmmain.js
@@ -1,22 +1,28 @@
 const openButton = document.querySelector(".result_table");
 const closeButton = document.querySelector(".close_btn");
 const result = document.querySelector(".result");
+const result_text = document.querySelector("#result_excercise");
 const modal = document.querySelector("#modal");
 
 const coefficient = {
-  squat: [0, 1, 1.0475, 1.13, 1.1575, 1.2, 1.242, 1.284, 1.326, 1.368, 1.41],
-  benchpress: [0, 1, 1.035, 1.08, 1.115, 1.15, 1.18, 1.22, 1.255, 1.29, 1.325],
-  deadlift: [0, 1, 1.065, 1.13, 1.147, 1.164, 1.181, 1.198, 1.22, 1.232, 1.24]
+  스쿼트: [0, 1, 1.0475, 1.13, 1.1575, 1.2, 1.242, 1.284, 1.326, 1.368, 1.41],
+  벤치프레스: [0, 1, 1.035, 1.08, 1.115, 1.15, 1.18, 1.22, 1.255, 1.29, 1.325],
+  데드리프트: [0, 1, 1.065, 1.13, 1.147, 1.164, 1.181, 1.198, 1.22, 1.232, 1.24]
 }
 
+
+// var exercise_value = document.getElementById("exercise");
+// var exercise_option =  exercise_value.options[exercise_value.selectedIndex].value);
+
 function calculate(exercise, reps, weights) {
-  var rm1 = weights * ((coefficient[exercise])[reps]);
-  var data = [];
+  let rm1 = weights * ((coefficient[exercise])[reps]);
+  let data = [];
   for (let i = 1; i < 11; i++) {
     data[i] = (rm1 / coefficient[exercise][i]).toFixed(2);
   }
   return data;
   result.style.display = "none";
+
 }
 
 function result_element(rm, weights) {
@@ -26,10 +32,21 @@ function result_element(rm, weights) {
     "</div>");
 }
 
+
+function result_exercise_text(exercise){
+  return ("<div class='result_exercise_text'>" +
+  "<p class='exercise'>" + "나의 "+ exercise+  " 1RM은 입니다." + "</p>" +
+  "</div>");
+}
+
+
+
 function action() {
   const exercise = $('select[name=exercise]').val();
   const reps = parseInt($('select[name=reps]').val());
   const weights = parseFloat($('input[name=weights]').val());
+
+
   if (!exercise || !reps || !weights) {
     alert("모든 항목을 정확히 입력해주세요");
     return;
@@ -40,16 +57,35 @@ function action() {
     return;
   }
 
+    $('.result-element').remove();
+    $('.result_exercise_text').remove();
 
-  $('.result-element').remove();
 
-  var list = calculate(exercise, reps, weights);
+  let list = calculate(exercise, reps, weights);
+
+  $('#result_excercise').append(result_exercise_text(exercise));
 
   for (let i = 1; i < 11; i++) {
-    console.log(i);
-    $('#list').append(result_element(i, list[i]));
+    // console.log(i);
+
+  $('#list').append(result_element(i, list[i]));
+
   }
+
+
+
+  console.log(list);
+  showExercise();
+
+  result_text.style.display = "flex";
   result.style.display = "flex";
+
+}
+
+function showExercise(){
+  let ex = document.getElementById("exercise");
+  let ex_1 = ex.options[ex.selectedIndex].value;
+  // console.log(ex_1);
 }
 
 function openModal(){
