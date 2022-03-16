85fe5ce3f77ac9605eecbde60b140d6572260d72
juni0914 <100837725+juni0914@users.noreply.github.com>
2022-03-16 23:56:22 +0900

    2022-03-16 / 23:56
    체크박스 삽입 및 인풋 값에 따른 이미지 출력 기능 구현

--- 1RMCal-main/js/1rmmain.js
+++ 1RMCal-main/js/1rmmain.js
@@ -1,5 +1,6 @@
 const openButton = document.querySelector(".result_table");
 const closeButton = document.querySelector(".close_btn");
+const gender_check = document.querySelector("#gender_checkbox");
 const result = document.querySelector(".result");
 const result_text = document.querySelector("#result_excercise");
 const modal = document.querySelector(".modal");
@@ -10,9 +11,16 @@ const coefficient = {
   데드리프트: [0, 1, 1.065, 1.13, 1.147, 1.164, 1.181, 1.198, 1.22, 1.232, 1.24]
 }
 
+function checkOnlyOne(element) {      //체크박스 중복방지 클릭 함수
+  const checkboxes
+      = document.getElementsByName("gender_check");
+  checkboxes.forEach((cb) => {
+    cb.checked = false;
+  })
+  element.checked = true;
+}
 
-
-function calculate(exercise, reps, weights) {
+function calculate(exercise, reps, weights) {      //1rm 공식 계산 함수
   let rm1 = weights * ((coefficient[exercise])[reps]);
   let data = [];
   for (let i = 1; i < 11; i++) {
@@ -24,7 +32,7 @@ function calculate(exercise, reps, weights) {
 
 }
 
-function result_element(rm, weights) {
+function result_element(rm, weights) {    // 계산 후 값 리스트로 출력하는 함수
   return ("<div class='result-element'>" +
     "<p class='re-rm'>" + rm + "RM</p>" +
     "<p class='re-we'>" + weights + "KG</p>" +
@@ -32,7 +40,7 @@ function result_element(rm, weights) {
 }
 
 
-function result_exercise_text(exercise, list){
+function result_exercise_text(exercise, list){    //input값 받아서 정보 출력하는 함수
   return ("<div class='result_exercise_text'>" +
   "<p class='exercise'>" + "나의 "+ exercise +
   " 1RM은 "+   "<span class='rm_text'>" + list[1] + "KG" +
@@ -40,7 +48,7 @@ function result_exercise_text(exercise, list){
 }
 
 
-function action() {
+function action() {      //계산하기 버튼 클릭 후 함수
   const exercise = $('select[name=exercise]').val();
   const reps = parseInt($('select[name=reps]').val());
   const weights = parseFloat($('input[name=weights]').val());
@@ -72,30 +80,34 @@ function action() {
   }
 
   console.log(list);
+  console.log(gender_check.value);
 
   result_text.style.display = "flex";
   result.style.display = "flex";
 }
 
 
-function openModal(){
-  if(exercise.value == "데드리프트"){
-    document.getElementById("table_img").src = "img/표3.png";
+function openModal(){ //성별과 운동종목을 넘겨받아서 각각의 모달창을 반환하는 함수
+  if(gender_check.value == "남성"){
+    if(exercise.value == "스쿼트") document.getElementById("table_img").src = "img/남성스쿼트.png";
+    else if(exercise.value == "벤치프레스") document.getElementById("table_img").src = "img/남성벤치프레스.png";
+    else if(exercise.value == "데드리프트") document.getElementById("table_img").src = "img/남성데드리프트.png";
   }
-  else if(exercise.value == "스쿼트"){
-    document.getElementById("table_img").src = "img/표1.png";
+
+  else{
+    if(exercise.value == "스쿼트") document.getElementById("table_img").src = "img/여성스쿼트.png";
+    else if(exercise.value == "벤치프레스") document.getElementById("table_img").src = "img/여성벤치프레스.png";
+    else if(exercise.value == "데드리프트") document.getElementById("table_img").src = "img/여성데드리프트.png";
   }
-  else if(exercise.value == "벤치프레스"){
-    document.getElementById("table_img").src = "img/표5.png";
-  }
+
   modal.style.display = 'flex';
 }
 
-function closeModal(){
+function closeModal(){    //모달창 닫는 함수
   modal.style.display = 'none';
 }
 
-window.onclick = function (event){
+window.onclick = function (event){  //모달 창 바깥 부분 클릭시 모달 창 닫는 함수
   if(event.target == modal){
     closeModal();
   }
