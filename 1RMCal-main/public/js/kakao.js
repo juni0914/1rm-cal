04ea73cd6b8aaf8acd2cc505793f238fde444860
juni0914 <100837725+juni0914@users.noreply.github.com>
2022-04-04 22:11:04 +0900

    2022-04-04 / 22:11
    카카오 공유기능 오류 수정 / 전반적인 코드 수정

--- 1RMCal-main/public/js/kakao.js
+++ 1RMCal-main/public/js/kakao.js
@@ -1,12 +1,12 @@
-const url = 'https://1rmcalculator-juni0914.netlify.app/';
+
 
 function kakaoShare(){
-
+  const url = 'https://rm-cal.web.app';
   Kakao.Link.sendDefault({
     objectType: 'feed',
       content: {
         title: '1RM 계산기',
-        description: '#헬스 #3대운동 #보디빌딩 #1RM #스쿼트 #바디프로필',
+        description: '#헬스 #3대운동 #보디빌딩 #바디프로필',
         imageUrl: 'https://i.ibb.co/hm13S2D/og-image.webp',
         link: {
           mobileWebUrl: url,
